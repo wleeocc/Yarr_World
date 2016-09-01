36439dc1429ab7d23253f8164969f1944305d944
wleeocc <wlee44@student.cccd.edu>
2016-09-01 16:49:50 -0700

    9/1/16

--- app/src/main/java/edu/orangecoastcollege/cs273/wlee/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/wlee/yarrworld/MainActivity.java
@@ -1,13 +1,24 @@
 package edu.orangecoastcollege.cs273.wlee.yarrworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Define the button as an instance variable (member variable)
+    Button speakButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // REstoring user settings from a previous session
         super.onCreate(savedInstanceState);
+        // Inflating (!) opening the layout called activity_main (activity_main.xml)
         setContentView(R.layout.activity_main);
+
+        // Hook up the speakButton to the one in our view;
+        speakButton = (Button) findViewById(R.id.speakButton);
+
     }
 }
