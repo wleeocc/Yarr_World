c044e3676eba78908fe1a667ba969f5a94fae78e
Wonseop Lee <wonseop.s.lee@gmail.com>
2016-09-06 16:44:51 -0700

    9/6/2016 Week2

--- app/src/main/java/edu/orangecoastcollege/cs273/wlee/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/wlee/yarrworld/MainActivity.java
@@ -1,24 +1,46 @@
 package edu.orangecoastcollege.cs273.wlee.yarrworld;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // Define the button as an instance variable (member variable)
-    Button speakButton;
+    private Button speakButton;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        // REstoring user settings from a previous session
+        // Restoring user settings from a previous session
         super.onCreate(savedInstanceState);
         // Inflating (!) opening the layout called activity_main (activity_main.xml)
         setContentView(R.layout.activity_main);
 
         // Hook up the speakButton to the one in our view;
         speakButton = (Button) findViewById(R.id.speakButton);
+
+        // Wire up the speakButton to perform an onClick event
+        speakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Use the static reference to "Toast" to display the message
+                // Toast = vanishes after set amount of time
+                // Dialog- requires user input
+                Toast.makeText(context, R.string.toast_text, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        speakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 }
